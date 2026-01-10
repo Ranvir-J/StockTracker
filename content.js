1db6c77263b90d8c747e2245c0893765d1884e93
Ranvir  Jhajj <jhajjr1@mcmaster.ca>
2026-01-10 14:03:29 -0500

    fixed scope error

--- content.js
+++ content.js
@@ -1,43 +1,46 @@
 console.log("✅ content.js loaded");
 
 function extractCartItems() {
-
-  document.querySelectorAll(".orderitemsummarytable").forEach(table => {
+    const tables = document.querySelectorAll(".orderitemsummarytable");
     if (!tables.length) {
     console.log("cart table not found yet");
     return;
     }
-
-    console.log(table);
+    
     const items = [];
 
+    tables.forEach(table => {
+        console.log("table found:", table);
 
-    table.querySelectorAll("tbody tr").forEach(row => {
-        const cells = row.querySelectorAll("td")
+        table.querySelectorAll("tbody tr").forEach(row => {
+            const cells = row.querySelectorAll("td")
 
-        const partNumber = cells[1]?.innerText.trim()
-        const quantity = parseInt(
-            row.querySelector(".centered")?.innerText.trim()
-        );
-
-        items.push({partNumber, quantity});
+            const partNumber = cells[1]?.innerText.trim()
+            const quantity = parseInt(
+                row.querySelector(".centered")?.innerText.trim()
+            );
+            if (partNumber && !isNaN(quantity)) {
+                items.push({ partNumber, quantity });
+            }
+        });
+        console.log("items found:", items)
     });
-    console.log(items)
-  });
 
-  chrome.runtime.sendMessage({
-    type: "CART_UPDATE",
-    payload: items
-  });
+    chrome.runtime.sendMessage({
+        type: "CART_UPDATE",
+        payload: items
+    });
 }
 
-extractCartItems();
+// extractCartItems();
 
 const observer = new MutationObserver(() => {
   extractCartItems();
 });
 
-observer.observe(document.querySelector("#container"), {
+const container = document.querySelector("#container") || document.body;
+
+observer.observe(container, {
   childList: true,
   subtree: true
 });
