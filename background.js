9be9b1ffc5f0e27eb3b19a6dc41bbd6864d17355
Ranvir  Jhajj <jhajjr1@mcmaster.ca>
2026-01-10 20:00:19 -0500

    popup functionality

--- background.js
+++ background.js
@@ -1,5 +1,20 @@
 chrome.runtime.onMessage.addListener((msg) => {
   if (msg.type === "STOCK_UPDATE") {
     console.log("background received cart update:", msg.payload);
+    const jsonPayload = JSON.stringify(msg.payload);
+    console.log(jsonPayload);
   }
 });
+
+let latestStockData = null;
+
+chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
+  if (msg.type === "STOCK_UPDATE") {
+    latestStockData = msg.payload;
+  }
+
+  if (msg.type === "GET_STOCK_DATA") {
+    sendResponse(latestStockData);
+  }
+});
+
--- popup.js
+++ popup.js
@@ -1,12 +1,35 @@
-// chrome.runtime.onMessage.addListener((msg) => {
-//   if (msg.type === "STOCK_UPDATE") {
-//     console.log("popup received cart update:", msg.payload);
-//     extractData();
-//   }
-// });
+chrome.runtime.sendMessage(
+  { type: "GET_STOCK_DATA" },
+  (data) => {
+    if (data) {
+      extractData(data);
+    }
+  }
+);
 
-// function extractData() {
-//     for (let i = 0; i <= msg.length; i++) {
-//      msg[i][0];}
+function extractData(data) {
+    const table = document.querySelector("tbody");
 
-// }
+    table.textContent = "";
+
+    data.forEach(item => {
+        const row = document.createElement("tr");
+
+        const partNum = document.createElement("td");
+        partNum.className = "data temporary";
+        partNum.textContent = item.partNumber;
+
+        const quantity = document.createElement("td");
+        quantity.className = "data temporary";
+        quantity.textContent = item.quantity;
+
+        row.append(partNum, quantity);
+        table.append(row);
+    });
+}
+
+document.getElementById("view-stock-btn").addEventListener("click", () => {
+  chrome.tabs.create({
+    url: chrome.runtime.getURL("fulltab.html")
+  });
+});
