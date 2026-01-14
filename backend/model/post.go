0940a16074db907d82ff3ec45499b7a346fa5361
Jason <pixel@TOASTER-MINI.localdomain>
2026-01-14 15:21:08 -0500

    Post model, repository implements

--- backend/model/post.go
+++ backend/model/post.go
@@ -3,8 +3,8 @@ package model
 import "time"
 
 type Post struct { // post = create (?)
-	partnum   string
-	quantity  int
-	createdAt time.Time
-	updatedAt time.Time
+	Partnum   string
+	Quantity  int
+	CreatedAt time.Time
+	UpdatedAt time.Time
 }
--- backend/repository/post_repo.go
+++ backend/repository/post_repo.go
@@ -8,7 +8,7 @@ import (
 type PostRepository interface {
 	Save(ctx context.Context, post model.Post)
 	Update(ctx context.Context, post model.Post)
-	Delete(ctx context.Context, post model.Post)
-	FindById(ctx context.Context, post model.Post) (model.Post, error)
-	GetAll(ctx context.Context, post model.Post) []model.Post
+	Delete(ctx context.Context, postId string)
+	FindById(ctx context.Context, postId string) (model.Post, error)
+	GetAll(ctx context.Context) []model.Post
 }
